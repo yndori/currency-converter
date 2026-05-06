74eb3b38df7d124de910ba4c56c9b21d0c28b473
sarmozo <yoannndori@gmail.com>
2026-05-06 22:35:35 +0000

    Updated the main

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import converter
 def main():
-    print("================Welcome to the Currency Converter================\n")
-    base_curency = str(input("Enter the base currency code: "))
-    target_curency = str(input("Enter the target currency code: "))
-    amount = float(input("Enter the amount you want to convert: "))
-    converter.convert(base_curency,target_curency,amount)
-    print("\n")
-    print("Do you want to convert another currency? (yes/no)")
-    answer = input()
-    if answer == 'yes':
-        main()
-    else:
-        print("\n================Thanks for using our service================")
+    while True:
+        print("================Welcome to the Currency Converter================\n")
+        base_curency = str(input("Enter the base currency code: "))
+        target_curency = str(input("Enter the target currency code: "))
+        amount = float(input("Enter the amount you want to convert: "))
+        converter.convert(base_curency,target_curency,amount)
+        print("\n")
+        print("Do you want to convert another currency? (yes/no)")
+        answer = input()
+        if answer.lower() != 'yes':
+            print("\n================Thanks for using our service================")
+            break
 
 if __name__ == "__main__" :
     main()
